3d2fc7c51066a84ca6006588722365b68f8bf265
leblance7 <leblance7@gmail.com>
2025-03-05 18:32:13 -0600

    Adding finished version of ContLangAry.cpp

--- ContLibAry.cpp
+++ ContLibAry.cpp
@@ -1,13 +1,23 @@
-#include <iostream>
+#include <iostream> 
 #include <array>
 
 int main() {
   /*Container Library array*/
-  int my_ary 3 > {9, 19, 29};
   std::array<int, 3> my_ary = {9, 19, 29};
+
+  /* Printing of the array before the array is incremented
+    - For loops without brackets are based on indentation.
+  */
+  for (int elem: my_ary) 
+    std::cout << "elem: " << elem << std::endl;
+  std::cout << "Printing of array before the array is incremented" << std::endl;
   for( int i = 0; i <= my_ary.size(); ++i) {
     ++my_ary[i];
   }
+  /*Enchanched for loops
+   - Must be done in the main method or will cause pointer error. */
+  for (int elem: my_ary) 
+    std::cout << "elem: " << elem << std::endl;
   // prt_ary(my_ary);
   
   std::cout << "Pikachu, you're supposed to go in the\
