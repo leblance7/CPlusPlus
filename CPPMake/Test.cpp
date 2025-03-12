14dcfdd9cc1447599209a4686bc793f6c05b867d
leblance7 <leblance7@gmail.com>
2025-03-12 18:07:21 -0500

    Adding and folder and example and the make file

---
+++ CPPMake/Test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::cout << "Inside of test file right now" << std::endl;
+  std::cout << "Inside of test file right now" << std::endl;
+  return 0;
+  }
+
