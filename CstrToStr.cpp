a7656abd84ee1dd44f1c099b04b4a6580ead6f4d
leblance7 <leblance7@gmail.com>
2025-03-12 19:22:06 -0500

    Finishing on character sequences, completing Makefile, and starting on pointers/

--- CstrToStr.cpp
+++ CstrToStr.cpp
@@ -6,5 +6,7 @@ int main() {
         std::string mystring = myntcs; // Converts c - string to string
 //        std:: cout << mystring;
         std::cout << "Converting c - sting, to string" << std::endl;
+        std::cout << mystring << std::endl;
+        std::cout << mystring.c_str() << std::endl;
         return 0;
 }
