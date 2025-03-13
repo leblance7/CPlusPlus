2816617854eaec4a1d148d1b278d084d57b45463
leblance7 <leblance7@gmail.com>
2025-03-12 19:13:26 -0500

    Most recent update

--- CstrToStr.cpp
+++ CstrToStr.cpp
@@ -2,6 +2,9 @@
 #include <string>
 
 int main() {
+        char myntcs[] = "Some text";
+        std::string mystring = myntcs; // Converts c - string to string
+//        std:: cout << mystring;
         std::cout << "Converting c - sting, to string" << std::endl;
         return 0;
 }
