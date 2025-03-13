d4796d62126359305d1199888b91a8770ae971b1
leblance7 <leblance7@gmail.com>
2025-03-13 18:41:11 -0500

    Adding the pointer section of CPLusPlus tutorial, and soon to add each section caterogized

---
+++ pointer/pointer.cpp
@@ -0,0 +1,8 @@
+
+
+#include <iostream>
+
+int main() {
+        std::cout << "Esther 4:14\n 'If you remain silent at this time, relief and deliverance for the Jews will arise from another place, but you and your father's family will perish. And who knows but that you have come into your royal position for sucha time as this?'" << std::endl;
+        return 0;
+}
