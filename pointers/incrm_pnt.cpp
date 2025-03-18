af4d7137a6ef35c5f6fb1d9e5dae403e3d3dbadf
leblance7 <leblance7@gmail.com>
2025-03-18 14:09:16 -0500

    Adding more files to the Makefile of pointers and allowing multiple to compile and execute.

---
+++ pointers/incrm_pnt.cpp
@@ -0,0 +1,19 @@
+/**
+* Syntax example of pointers and how to increment them. */        
+#include <iostream>
+
+int main(){
+//        int p = 7;
+//        *p++; // same as *(p++): increment pointer, and dereference unincremented address.
+//
+//        *++p; // same as *(p++): increment pointer, and dereference incremented address
+//        
+//        ++*p; // dereference pointer, and inrement the value it points to
+//
+//        (*p)++; // dereference pointer, and post - increment the value it points to. 
+        std::cout << "All of the days of my life i will praise the Lord." << std::endl;
+        return 0;
+}
+
+
+
---
+++ pointers/pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+        int firstvalue, secondvalue;
+        int * mypointer;
+        
+//Both are the same, or clos e by one decimal..
+        std::cout << "mypointer address: " << &mypointer << std::endl;
+        std::cout << "mypointer value: " << mypointer << std::endl;
+        mypointer = &firstvalue;
+        *mypointer = 10;
+        
+//Literal value of variable
+        std::cout << "firstvalue is " << firstvalue << std::endl;
+//Memory address of variable       
+        std::cout << "firstvalue memeory address is: " << firstvalue << std::endl;
+
+//secondvalue
+        mypointer = &secondvalue;
+        *mypointer = 20;
+
+        std::cout << "second value is: " << secondvalue << std::endl;
+
+        std::cout << "\n\nEsther 4:14\n 'If you remain silent at this time, relief and deliverance for the Jews will arise from another place, but you and your father's family will perish. And who knows but that you have come into your royal position for sucha time as this?'" << std::endl;
+        return 0;
+}
